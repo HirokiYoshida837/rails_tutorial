c3d9bee677f3dec4484aefedafd623f779280194
h.yoshida <hiroki.yoshida01@outlook.jp>
2023-08-20 12:12:11 +0900

    initialize rails

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+
+    def hello
+        render html: "hola, mundo!"
+    end
+
+    def goodbye
+        render html: "goodbye, world!"
+    end
+
+end
